c8987d9b24de4973d96b9c2257522f05920be3f9
Valerio <m.valerio9109@gmail.com>
2025-12-02 12:56:26 +0100

    fixed web. fixed window sizes for different platforms

--- composeApp/src/commonMain/kotlin/homeaq/dothattask/dothattask_fe/dothattask_fe/View/CompletedTaskPage.kt
+++ composeApp/src/commonMain/kotlin/homeaq/dothattask/dothattask_fe/dothattask_fe/View/CompletedTaskPage.kt
@@ -1,6 +1,7 @@
 package homeaq.dothattask.dothattask_fe.dothattask_fe.View
 
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,7 +22,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import homeaq.dothattask.dothattask_fe.dothattask_fe.Model.Task
 import homeaq.dothattask.dothattask_fe.dothattask_fe.Network.ApiResult
 import homeaq.dothattask.dothattask_fe.dothattask_fe.Network.TaskApi
@@ -83,10 +86,13 @@ fun CompletedTaskPage() {
         {
             Row(modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 2.dp, vertical = 4.dp),
-                verticalAlignment = Alignment.Top)
+                .padding(horizontal = 2.dp, vertical = 15.dp),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Center)
             {
-                Text("No tasks completed yet", modifier = Modifier.size(25.dp))
+                Text("No tasks completed yet", style = MaterialTheme.typography.bodyLarge,
+                    color = Color.Gray,
+                    fontSize = 30.sp)
             }
         }
         if(tasks.isNotEmpty())
--- composeApp/src/jvmMain/kotlin/homeaq/dothattask/dothattask_fe/dothattask_fe/main.kt
+++ composeApp/src/jvmMain/kotlin/homeaq/dothattask/dothattask_fe/dothattask_fe/main.kt
@@ -1,13 +1,19 @@
 package homeaq.dothattask.dothattask_fe.dothattask_fe
 
+import androidx.compose.ui.unit.dp
 import homeaq.dothattask.dothattask_fe.dothattask_fe.View.App
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "dothattask_fe",
+        title = "Do That Task!",
+        state = rememberWindowState(
+            width = 600.dp,
+            height = 900.dp
+        )
     ) {
         App()
     }
